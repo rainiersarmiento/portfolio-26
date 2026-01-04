d93650ff64434a2486b4739a59ac26517fab453b
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2026-01-04 00:18:46 -0800

    Added Spline avatar and dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { ComponentExample } from "@/components/component-example";
 import Navbar from "./components/Navbar";
+import Avatar from "./components/Avatar";
 export function App() {
   return (
     <>
       <Navbar />
+      <Avatar />
       <ComponentExample />
     </>
   );
--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -1,14 +1,12 @@
 import Spline from "@splinetool/react-spline";
 
-export default function App() {
+const Avatar = () => {
   return (
     <>
       {/* <Spline scene="https://prod.spline.design/qRl3rq0ulMJ7toHL/scene.splinecode" /> */}
       <Spline scene="https://prod.spline.design/qRl3rq0ulMJ7toHL/scene.splinecode" />
     </>
   );
-}
-const Avatar = () => {
-  return <></>;
 };
+
 export default Avatar;

92b99177f3c2613651801493c7e64e1471d77a1c
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2026-01-05 09:33:51 -0800

    updates to landing section

--- frontend/src/sections/Landing.tsx
+++ frontend/src/sections/Landing.tsx
@@ -5,19 +5,27 @@ import Avatar from "@/components/Avatar";
 const Landing = () => {
   // Cycle through text using useEffect
   const intro: Array<string> = useMemo(
-    () => ["Web Developer", "Software Engineer"],
+    () => ["Web Developer", "Software Engineer", "IT Technician", "Explorer"],
     []
   );
   const [index, setIndex] = useState(0);
   useEffect(() => {
-    const timer = setTimeout(() => setIndex(index === 0 ? 1 : 0), 2500);
+    const timer = setTimeout(
+      () =>
+        index < intro.length - 1
+          ? setIndex((prevIndex) => prevIndex + 1)
+          : setIndex(0),
+      2500
+    );
     return () => clearTimeout(timer);
   }, [intro, index]);
   return (
     <section id="landing">
       <Avatar />
-      <h1>Rainier Sarmiento, </h1>
-      <h2>{intro[index]}</h2>
+      <h1>
+        <strong>Rainier Sarmiento</strong>,{" "}
+      </h1>
+      <h2 key={index}>{intro[index]}</h2>
     </section>
   );
 };

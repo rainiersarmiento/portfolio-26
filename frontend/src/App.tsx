032ad0022bef3c6f95a8c88b41b88c50b521327a
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2026-01-05 07:28:37 -0800

    base Contact section added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { ComponentExample } from "@/components/component-example";
 import Navbar from "./components/Navbar";
 import Landing from "./sections/Landing";
-
+import Contact from "./sections/Contact";
 export function App() {
   return (
     <>
       <Navbar />
       <Landing />
+      <Contact />
       <ComponentExample />
     </>
   );
---
+++ frontend/src/sections/Contact.tsx
@@ -0,0 +1,8 @@
+const Contact = () => {
+  return (
+    <section>
+      <h1>Contact Me</h1>
+    </section>
+  );
+};
+export default Contact;

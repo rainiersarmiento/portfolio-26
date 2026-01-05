85bf7303c095dc63443e4407b154db90f35f560e
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2026-01-05 07:17:32 -0800

    Completed landing section with no styling

--- frontend/src/sections/Landing.tsx
+++ frontend/src/sections/Landing.tsx
@@ -1,10 +1,24 @@
+import { useState, useEffect, useMemo } from "react";
+
 import Avatar from "@/components/Avatar";
+
 const Landing = () => {
+  // Cycle through text using useEffect
+  const intro: Array<string> = useMemo(
+    () => ["Web Developer", "Software Engineer"],
+    []
+  );
+  const [index, setIndex] = useState(0);
+  useEffect(() => {
+    const timer = setTimeout(() => setIndex(index === 0 ? 1 : 0), 2000);
+    return () => clearTimeout(timer);
+  }, [intro, index]);
   return (
     <>
       <div>
         <Avatar />
-        <h1>Rainier Sarmiento</h1>
+        <h1>Rainier Sarmiento, </h1>
+        <h2>{intro[index]}</h2>
       </div>
     </>
   );

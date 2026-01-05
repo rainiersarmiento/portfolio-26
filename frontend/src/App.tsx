432206b2c38a43359d5006c39bc7c7a14aa9bd18
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2026-01-05 07:30:24 -0800

    base about section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { ComponentExample } from "@/components/component-example";
 import Navbar from "./components/Navbar";
 import Landing from "./sections/Landing";
 import Contact from "./sections/Contact";
+import About from "./sections/About";
 export function App() {
   return (
     <>
       <Navbar />
       <Landing />
+      <About />
       <Contact />
       <ComponentExample />
     </>
